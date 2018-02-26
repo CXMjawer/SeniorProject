a42cfca5bb986ae518a51ec42e1dfe3d5a0bce24
CXMjawer <35793840+CXMjawer@users.noreply.github.com>
2018-02-26 15:36:14 +0300

    Week 6

---
+++ Scripts/LoadGarden.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadGarden : MonoBehaviour {
+
+	void OnTriggerEnter(Collider Col){
+		if (Col.gameObject.tag == "Player") {
+			Application.LoadLevel ("Garden");
+		}
+	}
+
+}
---
+++ Scripts/LoadMosque.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadMosque : MonoBehaviour {
+
+	void OnTriggerEnter(Collider Col){
+		if (Col.gameObject.tag == "Player") {
+			Application.LoadLevel ("Mosque");
+		}
+	}
+
+}
